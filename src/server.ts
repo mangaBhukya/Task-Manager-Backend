a5234441b6bc8b35db5ac7b3496280c8cc5c53e5
Manga <mangab617@gmail.com>
2025-02-22 16:12:48 +0530

    connect to the database

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,22 @@
 
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
 
+
+
+dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-const PORT = 8000;
+
+const PORT = process.env.PORT || 8000;
+
+connectDB().then(()=> {
 app.listen(PORT,()=> console.log(`server running on port ${PORT}`))
+
+});
