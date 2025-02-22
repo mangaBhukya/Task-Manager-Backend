1c05300ce49a50ebbe9ff498a0aeeb4d4206e041
Manga <mangab617@gmail.com>
2025-02-22 18:43:05 +0530

    Added route

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
+import taskRoutes from "./routes/taskRoutes";
 
 
 
@@ -10,10 +11,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 
+
+app.use("/api/tasks", taskRoutes);
 const PORT = process.env.PORT || 8000;
 
 connectDB().then(()=> {
